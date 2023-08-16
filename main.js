59e2dd9168960ab5dd383e1644c70de52ffa58d3
freaky4wrld <freaky4wrld@gmail.com>
2023-08-16 19:14:45 +0530

    added erase button and provided it the erasing functionality to erase grid content

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const gridContainer = document.querySelector('.container');
 const gridSmallBtn = document.getElementById('small-btn');
 const gridMediumBtn = document.getElementById('medium-btn');
 const gridLargeBtn = document.getElementById('large-btn');
-const gridClearBtn = document.getElementById('clear-btn')
+const gridClearBtn = document.getElementById('clear-btn');
+const gridBlackBtn = document.getElementById('black-btn');
+const gridEraseBtn = document.getElementById('erase-btn');
 gridSizeTogglerMain()
 gridClearBtn.addEventListener('click', eraseGrid);
 // provide size-changing functionality to buttons
@@ -13,14 +15,19 @@ function gridSizeTogglerMain(){
     gridLargeBtn.addEventListener('click', ()=>{resetGrid();createGrid(50,10)});    
 }
 
+function gridColorTogglerMain(){
+    colorGrid('#333');
+    gridBlackBtn.addEventListener('click', ()=>{colorGrid('black')})
+    gridEraseBtn.addEventListener('click', ()=>{colorGrid('white');})
+}
 
 
 // to color the grid
-function colorGrid(){
+function colorGrid(gridColor){
     var gridBox = document.querySelectorAll('.grid');
     Array.from(gridBox).forEach((item)=>{
     item.addEventListener('mouseenter', (event)=>{
-    event.target.style.backgroundColor = '#323';
+    event.target.style.backgroundColor = `${gridColor}`;
     })
 })
 }
@@ -38,7 +45,7 @@ function createGrid(gridCount,gridSize){
         gridContainer.style.gridTemplateColumns = `repeat(${gridCount}, 1fr)`;
         gridContainer.style.gridTemplateRows = `repeat(${gridCount}, 1fr)`;
     }
-    colorGrid()
+    gridColorTogglerMain()
 }
 
 // to remove the grid element while changing grid density
@@ -48,8 +55,23 @@ function resetGrid(){
     })
 }
 
+// to erase the grid content
 function eraseGrid(){
     Array.from(gridContainer.children).forEach((item)=>{
         item.style.backgroundColor = '#fff';
     })
 }
+
+// function shadeGrid(counter=0.2){
+//     colorGrid()
+//     Array.from(gridContainer.children).forEach((item)=>{
+//         item.addEventListener('mouseenter', (event)=>{
+//             console.log(counter);
+//             event.target.style.opacity = `${0.1+counter}`;
+//             console.log("event",event.target.style.opacity);
+//             counter+=0.15;
+//             console.log(counter);
+//             setTimeout(shadeGrid.bind(counter),100);
+//             })  
+//     })
+// }

6831b902caad515cf7be3dc19073f65755eb7f6a
freaky4wrld <freaky4wrld@gmail.com>
2023-08-16 18:09:17 +0530

    added more button options to the ui and provided the size buttons the size changing ability

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
 const gridContainer = document.querySelector('.container');
-const gridSizeChangeBtn = document.getElementById('btn');
-createGrid(16);
+const gridSmallBtn = document.getElementById('small-btn');
+const gridMediumBtn = document.getElementById('medium-btn');
+const gridLargeBtn = document.getElementById('large-btn');
 
+gridSizeTogglerMain()
+
+// provide size-changing functionality to buttons
+function gridSizeTogglerMain(){
+    createGrid(16,31.2);
+    gridSmallBtn.addEventListener('click', ()=>{resetGrid();createGrid(16,31.2)});
+    gridMediumBtn.addEventListener('click', ()=>{resetGrid();createGrid(32,15.6)});
+    gridLargeBtn.addEventListener('click', ()=>{resetGrid();createGrid(50,10)});    
+}
+
+
+// to color the grid
 function colorGrid(){
     var gridBox = document.querySelectorAll('.grid');
     Array.from(gridBox).forEach((item)=>{
     item.addEventListener('mouseenter', (event)=>{
-    event.target.style.backgroundColor = 'black';
+    event.target.style.backgroundColor = '#323';
     })
 })
 }
 
-gridSizeChangeBtn.addEventListener('click', ()=>{
-    let sizeValue = prompt('Enter a size: ');
-    createGrid(sizeValue);
-    gridContainer.style.width = '70%'
-})
-
-function createGrid(gridSize){
-    for (let index = 1; index <=gridSize; index++) {
-        for(let counter = 1; counter<=gridSize; counter++){
+// creating grid element and providing them sizing
+function createGrid(gridCount,gridSize){
+    for (let index = 1; index <=gridCount; index++) {
+        for(let counter = 1; counter<=gridCount; counter++){
             let grid = document.createElement('div');
             grid.className = 'grid';
             gridContainer.appendChild(grid)
+            grid.style.width = `${gridSize}px`;
+            grid.style.height = `${gridSize}px`
         }
+        gridContainer.style.gridTemplateColumns = `repeat(${gridCount}, 1fr)`;
+        gridContainer.style.gridTemplateRows = `repeat(${gridCount}, 1fr)`;
     }
     colorGrid()
+}
+
+// to remove the grid element while changing grid density
+function resetGrid(){
+    Array.from(gridContainer.children).forEach((item)=>{
+        item.remove();
+    })
 }
